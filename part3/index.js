a2a4d85cdb9e006c82410ca218b184c3f1af92b6
leop98 <150611962+leop98@users.noreply.github.com>
2025-01-27 16:59:05 +0200

    part3

--- part3/index.js
+++ part3/index.js
@@ -1,44 +1,113 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+morgan.token('body', (request) => JSON.stringify(request.body))
+
 let persons = [
-    { 
-      name: "Arto Hellas", 
-      number: "040-123456",
-      id: "1"
+    {
+      "id": "1",
+      "name": "Arto Hellas",
+      "number": "040-123456"
     },
-    { 
-      name: "Ada Lovelace", 
-      number: "39-44-5323523",
-      id: "2"
+    {
+      "id": "2",
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
     },
-    { 
-      name: "Dan Abramov", 
-      number: "12-43-234345",
-      id: "3"
+    {
+      "id": "3",
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
     },
-    { 
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122",
-      id: "4"
-    }
+    {
+      "id": "4",
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }        
   ]
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
   
-  app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`)
   })
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(note => note.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+      : 0
+  return String(maxId + 1)
+}
+
+const postMorgan = morgan(':method :url :status :res[content-length] - :response-time ms :body')
   
-  app.get('/info', (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>`
-      )
+app.post('/api/persons', postMorgan, (request, response) => {
+  const body = request.body
+
+  if (!body.name && !body.number) {
+    return response.status(400).json({
+      error: 'name and number missing',
     })
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+  }
+  
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  if (persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())) {
+    return response.status(400).json({ 
+      error: 'person exists already' 
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+  
+  persons = persons.concat(person)
+  
+  response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
